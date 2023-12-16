e776b765cadae076793248135a33c857d0bf3733
Alex S <insten490@gmail.com>
2023-12-15 18:08:11 -0600

    finished login function

--- src/gateway/server.py
+++ src/gateway/server.py
@@ -16,3 +16,8 @@ channel = connection.channel()
 @server.route("/login", methods=["POST"])
 def login():
     token, err = access.login(request)
+
+    if not err:
+        return token
+    else:
+        return err
